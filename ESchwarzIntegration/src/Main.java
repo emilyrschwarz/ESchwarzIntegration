19b545be934e1fd773a9563b59a6a93f7366924f
erschwarz6195 <erschwarz6195@L438R4S7.STUDENT.ad.fgcu.edu>
2018-01-25 18:38:02 -0500

    First commit!

---
+++ ESchwarzIntegration/src/Main.java
@@ -0,0 +1,13 @@
+// Emily Schwarz
+// My Integration Project that integrates all the
+// skills I've learned in my Intro to Programming class
+// COP 2006
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		// sysout Ctrl+space 
+	}
+
+}
