18688d89bf4b2a9da13403b866a9cf11e3ba3671
erschwarz6195 <erschwarz6195@L438R4S7.STUDENT.ad.fgcu.edu>
2018-01-25 19:08:19 -0500

    Added comment and inputs

--- ESchwarzIntegration/src/Main.java
+++ ESchwarzIntegration/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 // Emily Schwarz
 // My Integration Project that integrates all the
 // skills I've learned in my Intro to Programming class
@@ -6,8 +7,22 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
-		// sysout Ctrl+space 
-	}
+		Scanner input = new Scanner(System.in);
+		System.out.println("Hello from Emily's Integration Project!");
+		
+		System.out.println("What is your name?");
+		String inputName = input.nextLine();
+		
+		// this variable allows the user to input their name
+		// a variable 
+		
+		System.out.println("Your name is " + inputName + "? How nice!");
+		
+		System.out.println("How old are you, " + inputName + "?");
+		int inputAge = input.nextInt();
+		
+		System.out.println("You're " + inputAge + "? Cool!");
+		
 
+	}
 }
