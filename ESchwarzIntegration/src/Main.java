104d0831598666012e23e81ee679ec52b3aecf81
erschwarz6195 <erschwarz6195@L438R4S7.STUDENT.ad.fgcu.edu>
2018-02-06 18:36:52 -0500

    added classes

--- ESchwarzIntegration/src/Main.java
+++ ESchwarzIntegration/src/Main.java
@@ -1,30 +1,44 @@
 import java.util.Scanner;
 // Emily Schwarz
 // My Integration Project that integrates all the
-// skills I've learned in my Intro to Programming class
-// COP 2006
+// skills I've learned in my Introduction to Programming class.
+// I love adventure style games, so I've decided to use what I've
+// learned in class to make something fun!
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.println("Welcome to Emily's Integration Project!");
+		System.out.println("Hello, welcome to my Integration Project!");
 		System.out.println("This is a text-based adventure game.");
-		// this is an intro to the game
+		// This is an introduction to the game
 
 		System.out.println("To begin, what is your name, adventurer?");
 		String playerName = input.nextLine();
 		// playerName allows the user to input their name
+		// Here is where I've used my first variable. A variable is a 
+		// a placeholder for something else, like a string, an integer,
+		// a character etc.
 		
 		System.out.println("Your name is " + playerName + "? How nice!");
 		System.out.println("Next, how old are you, " + playerName + "?");
 		int playerAge = input.nextInt();
-		//playerAge allows the user to input their name
+		// playerAge allows the user to input their name
+		// Java has a few different data types. They are the following:
+		// Byte: used to save memory in a small amount
+		// Short: same as a byte
+		// Int: to use integer data types as an unsigned integer
+		// Long: stores values larger than int
+		// Float: used when you have to store something with a precise value 
+		// (decimal)
+		// Double: for decimals (never precise)
+		// Boolean: only for true/false values
+		// Char: represents a single 16-bit Unicode character
 		
-		System.out.println("You're " + playerAge + "? Nice! People of all ages "
+		System.out.println("You're " + (int) playerAge + "? Nice! People of all ages "
 		    + "can play, so you're in luck!");
 		
-		//start game below
+		// start game below
 		System.out.println("Greetings " + playerName + "I am Robin, your mentor"
 		    + " for your journey.");
 		System.out.println("Please type 'Y', so I know you're ready!");
@@ -35,9 +49,13 @@ public class Main {
 		    + "companion.");
 		System.out.println("Pick an element of nature to get your new friend! "
 		    + "Please type 'water', 'air', 'fire', or 'earth'.");
+		String playerElementChoice = input.nextLine();
+		// playerElementChoice allows the player to type in element of their
+		// choosing. This allows them to pick a companion based off an element
 		
 		
-	
+		
+
 		
 
 	}
